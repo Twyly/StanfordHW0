41cf956aaa691cc5e437a23662989e38f157f56c
Teddy Wyly <wylynout@gmail.com>
2013-08-21 20:18:55 -0400

    first commit

---
+++ Matchismo/CardGameViewController.m
@@ -0,0 +1,55 @@
+//
+//  CardGameViewController.m
+//  Matchismo
+//
+//  Created by Teddy Wyly on 8/5/13.
+//  Copyright (c) 2013 Teddy Wyly. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
+
+@interface CardGameViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
+@property (strong, nonatomic) Deck *deck;
+
+@end
+
+@implementation CardGameViewController
+
+- (Deck *)deck
+{
+    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
+    return _deck;
+}
+
+- (void)setFlipCount:(int)flipCount
+{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+}
+
+- (IBAction)flipCard:(UIButton *)sender
+{
+    sender.selected = !sender.isSelected;
+    self.flipCount++;
+    
+    Card *card = [self.deck drawRandomCard];
+    [sender setTitle:card.contents forState:UIControlStateSelected];
+}
+
+#pragma mark - View Controller Life Cycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
